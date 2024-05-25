b7d2320d830404b6dccfe2d58375119d05267cb6
Ankit Chouhan <99067327+Chouhanankit@users.noreply.github.com>
2024-05-25 23:11:55 +0530

    cors updated

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 4000;
 
 app.use(
     cors({
-      origin: "http://localhost:5173",
+      origin: "https://support-frontend-theta.vercel.app/",
       methods: "GET, POST, PUT, DELETE",
       credentials: true,
     })
